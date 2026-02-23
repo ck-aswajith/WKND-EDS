a60c1c14c87d2ba9c1b4946f55f782cd4d1d73d0
ck-aswajith <inf44580@adobe.com>
2026-02-23 20:38:36 +0530

    dynamic listing articles from indexed url

---
+++ blocks/article-listing/article-listing.js
@@ -0,0 +1,51 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default async function decorate(block) {
+  let articles = [];
+  try {
+    const resp = await fetch(block.textContent);
+    if (resp.ok) {
+      const json = await resp.json();
+      articles = json.data || json.articles || [];
+    }
+  } catch (e) {
+    block.textContent = 'Failed to load articles.';
+    return;
+  }
+
+  // Create a list to display articles
+  if (articles.length > 0) {
+    const parentDiv = document.createElement('div');
+          parentDiv.classList.add('article-listing-row');
+    articles.forEach((article) => {
+      const link = document.createElement('a');
+      const itemDiv = document.createElement('div');
+            itemDiv.classList.add('article-listing-item');
+      const title = article.title || 'Untitled Article';
+      const url = article.path || '#';
+      const desc = article.description || 'lorem ipsum dolor sit amet.';
+      const p = document.createElement('picture');
+      const img = document.createElement('img');
+      img.src = article.image || '';
+      img.alt = title;
+      p.appendChild(img);
+      itemDiv.appendChild(p);
+      const h3 = document.createElement('h3');
+      h3.textContent = title;
+      itemDiv.appendChild(h3);
+      const d = document.createElement('p');
+      d.textContent = desc;
+      itemDiv.appendChild(d);
+      itemDiv.addEventListener('click', () => {
+      console.log('Navigating to:', url);
+      window.location.href = url;
+      });
+      parentDiv.appendChild(itemDiv);
+    });
+    parentDiv.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+    block.replaceChildren(parentDiv);
+  } else {
+    block.textContent = 'No articles found.';
+  }
+}
+
